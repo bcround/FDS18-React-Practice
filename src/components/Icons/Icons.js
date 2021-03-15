88d337950c628ba77b01181842aa779822e6de61
bcround <sbc1142@gmail.com>
2021-03-15 15:48:42 +0900

    feat: Add styling to Icons

--- src/components/Icons/Icons.js
+++ src/components/Icons/Icons.js
@@ -3,24 +3,38 @@ import { ReactComponent as Letter } from './images/letter.svg'
 import { ReactComponent as Lock } from './images/lock.svg'
 import { ReactComponent as Hide } from './images/hide.svg'
 import { ReactComponent as Show } from './images/show.svg'
+import { iconContainer, grayBorder } from './Icons.module.scss'
+import classNames from 'classnames'
 
 const Icons = ({ iconType, title, ...restProps }) => {
-  switch(iconType) {
-    case('letter'):
-      return <Letter title={title} {...restProps}/>
+  const composeClasses = classNames(
+    iconContainer,
+    iconType === 'letter' ? grayBorder: iconType === 'lock' ? grayBorder: '',
+  )
 
-    case('lock'):
-      return <Lock title={title} {...restProps}/>
+  return (
+  <div className={composeClasses}>{
+    (() => {
+      switch(iconType) {
+        case('letter'):
+          return <Letter className="letter" title={title} {...restProps}/>
 
-    case('hide'):
-      return <Hide title={title} {...restProps}/>
+        case('lock'):
+          return <Lock className="lock" title={title} {...restProps}/>
 
-    case('show'):
-      return <Show title={title} {...restProps}/>
+        case('hide'):
+          return <Hide title={title} {...restProps}/>
 
-    default: 
-      return 'Error'
+        case('show'):
+          return <Show title={title} {...restProps}/>
+
+        default: 
+          return 'Error'
+      }
+    })()
   }
+  </div>
+  )
 }
 
 Icons.propTypes = {
